379c11f0884538a90b6de2138eef16559bdbefa8
Vince Tell <vinyomester@gmail.com>
2025-12-02 19:58:06 +0100

    Infused Bonemeal!

--- src/main/java/net/tvinci/floramancy/item/ModItems.java
+++ src/main/java/net/tvinci/floramancy/item/ModItems.java
@@ -12,6 +12,7 @@ public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
     public static final Item SOUL_CRYSTAL = registerItem("soul_crystal", new Item(new Item.Settings()));
+    public static final Item INFUSED_BONEMEAL = registerItem("infused_bonemeal", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TVincisFloramancy.MOD_ID, name), item);
@@ -24,6 +25,7 @@ public class ModItems {
             fabricItemGroupEntries.add(PINK_GARNET);
             fabricItemGroupEntries.add(RAW_PINK_GARNET);
             fabricItemGroupEntries.add(SOUL_CRYSTAL);
+            fabricItemGroupEntries.add(INFUSED_BONEMEAL);
         });
     }
 }
