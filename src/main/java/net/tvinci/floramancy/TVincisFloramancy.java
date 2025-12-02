008b5b82653ed153ddc3d35ea1513fe0c1437805
Vince Tell <vinyomester@gmail.com>
2025-12-02 19:25:15 +0100

    Added items from the tutorial and also the soul crystal. Its time to start :D

--- src/main/java/net/tvinci/floramancy/TVincisFloramancy.java
+++ src/main/java/net/tvinci/floramancy/TVincisFloramancy.java
@@ -2,6 +2,7 @@ package net.tvinci.floramancy;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tvinci.floramancy.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class TVincisFloramancy implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/tvinci/floramancy/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.tvinci.floramancy.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.tvinci.floramancy.TVincisFloramancy;
+
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+    public static final Item SOUL_CRYSTAL = registerItem("soul_crystal", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TVincisFloramancy.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TVincisFloramancy.LOGGER.info("Registering Mod Items for "+TVincisFloramancy.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(PINK_GARNET);
+            fabricItemGroupEntries.add(RAW_PINK_GARNET);
+            fabricItemGroupEntries.add(SOUL_CRYSTAL);
+        });
+    }
+}
